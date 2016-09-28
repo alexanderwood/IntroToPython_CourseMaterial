0b221cf014e53ac4a89bb929f5557642b6f910c7
alexwood2 <awood@gradcenter.cuny.edu>
2016-09-28 17:25:01 -0400

    Boolean Expressions

---
+++ BooleanExpressions/py_files/empty_string_test.py
@@ -0,0 +1,12 @@
+# this program determines whether a string is empty.
+
+def main():
+    some_string = ''
+
+    if some_string:
+        print 'there is something in the string!'
+
+    else:
+        print 'there is nothing here!'
+
+main()
